a737e4a3ea467fb39fab9e4ce8d34a2ea7a8ac22
Valentina03 <konovalenko.valya.a@gmail.com>
2022-02-15 20:52:59 +0200

    lesson5

---
+++ SecondClass.java
@@ -0,0 +1,9 @@
+public class SecondClass {
+
+    public int PrintText() {
+        int value = 11111;
+        String text = "---%s---%n";
+        System.out.printf(text, value);
+        return value;
+    }
+}
